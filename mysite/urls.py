6885e78e4cfb166cbe5c6e167502a11c60c91bbb
tang1201 <1506273520@qq.com>
2021-02-24 16:50:36 +0800

    初始化项目1

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
-"""DjangoWeb URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from blog.views import blog_list
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('', blog_list, name='home'),
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('ckeditor', include('ckeditor_uploader.urls')),
     path('blog/', include('blog.urls')),
+    path('comment/', include('comment.urls')),
+    path('likes/', include('likes.urls')),
+    path('user/', include('user.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
